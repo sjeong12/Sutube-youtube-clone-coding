5ebd01903c525cbfaeaaf9ce732d400d9b08294b
sujlee <sujlee@student.42seoul.kr>
2024-05-11 15:28:12 +0900

    fix play video infinite

--- youtube/src/js/playlist.js
+++ youtube/src/js/playlist.js
@@ -10,14 +10,17 @@ let adlist = null;
 (async function () {
 	const videolistTxt = await readFile("playlist.txt");
 	const adlistTxt = await readFile("ad.txt");
-
+	let videoCnt = localStorage.getItem("videoCnt");
+	let prevAdCnt = localStorage.getItem("adPrevPlayCnt");
 
 	videolist = !videolistTxt ? [] : videolistTxt.split('\n');
 	adlist = !adlistTxt ? [] : adlistTxt.split('\n');
-	if(!localStorage.getItem('videoCnt')) localStorage.setItem("videoCnt", 1);
-	if(!localStorage.getItem('adPrevPlayCnt')) localStorage.setItem("adPrevPlayCnt", 0);
+	if(!videoCnt) videoCnt = 0;
+	if(!prevAdCnt) prevAdCnt = 0;
 
-	videoSource.setAttribute('src', `/data/video/${videolist[0]}`);
+	videoSource.setAttribute('src', `/data/video/${videolist[videoCnt]}`);
+	localStorage.setItem("videoCnt", ++videoCnt % videolist.length);
+	localStorage.setItem("adPrevPlayCnt", ++prevAdCnt % adlist.length);
 	video.load();
 })();
 
@@ -25,21 +28,16 @@ export function playNextVideo() {
 	let videoCnt = localStorage.getItem("videoCnt");
 	let prevAdCnt = localStorage.getItem("adPrevPlayCnt");
 
-	if (videoCnt >= videolist.length)
-		videoCnt = 0;
-	else if (videoCnt < 0)
-		videoCnt = videolist.length + parseInt(videoCnt);
-
 	setControllsState('hidden');
 	document.getElementById("playpause").setAttribute('data-state', 'play');
 	videoSource.setAttribute('src', `/data/video/${videolist[videoCnt]}`);
 	if (prevAdCnt < adlist.length)
-		showAdPreVideo (10000, adlist[prevAdCnt]);
+		showAdPreVideo (10000,  adlist[prevAdCnt]);
 	setTimeout(function () { video.load(); }, 10000);
 
 
 	log("클릭", "다음 영상 시청", videolist[videoCnt]);
-	localStorage.setItem("videoCnt", ++videoCnt);
-	localStorage.setItem("adPrevPlayCnt", ++prevAdCnt);
+	localStorage.setItem("videoCnt", ++videoCnt % videolist.length);
+	localStorage.setItem("adPrevPlayCnt", ++prevAdCnt % adlist.length);
 	localStorage.setItem("adInPlayCnt", 0);
 }
